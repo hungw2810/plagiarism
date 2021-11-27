c653a6c4e122bd88d6808f5e97bcd54ed2cd0dca
hungw2810 <hungnn2810@gmail.com>
2021-11-27 23:40:51 +0700

    27/11

--- data_func.py
+++ data_func.py
@@ -2,7 +2,7 @@ from enum import unique
 from io import StringIO
 import os
 from typing_extensions import final
-
+import pandas as pd
 from underthesea import word_tokenize
 from underthesea import sent_tokenize
 
@@ -12,6 +12,7 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 from pdfminer.converter import TextConverter
+from underthesea.pipeline.word_tokenize.regex_tokenize import tokenize
 
 
 def convert_pdf_to_string(file_path):
@@ -56,59 +57,13 @@ def remove_speChar(input_str):
         input_str=input_str.replace(char,'')
     return input_str
 
-def read_allTextFile():
-    text=''
-    for folder in os.listdir('data'):
-        for textFile in os.listdir('data/'+folder):
-            with open('data/'+folder+'/'+textFile,'r',encoding='utf-8') as f:
-                temp=f.read()
-                text+=temp+' '
-    return text
-
-def get_uniqueList(list_input):
-    unique=[]
-    for string in list_input:
-        if string not in unique:
-            unique.append(string)
-    return unique
-
-
-def generate_vocab(unique:bool,directory=None):  
-    final_vocal=[]
-    raw_vocab=[] # have undefined word
-    sent_array=[]
-
-    #read vietnamese dictionary 
-    with open('dic.txt','r',encoding='utf-8') as f:
-        temp_dict=f.read()
-        underthesea_words=temp_dict.split('\n')
-
-    #create global vocab
-    if directory is None:
-        sentences=read_allTextFile().split('.')
-        
-    elif directory is not None:
-        with open(directory,'r',encoding='utf-8') as f:
-            sentences=f.read().split('.')       
-
-    #remove spe characters
-    for i in range(len(sentences)):
-        if sentences[i] != '' and not sentences[i].isspace():
-            sent_array.append(remove_speChar(sentences[i]))
-
-    #tokenize word & append to raw vocab
-    for i in range (len(sent_array)):
-        temp=word_tokenize(sent_array[i],format='text')
-        sent_array[i]=temp.lower()
-        temp=sent_array[i].split()
-        raw_vocab.extend(temp)
-    for word in raw_vocab:
-        if word in underthesea_words:
-           final_vocal.append(word) 
-
-# 2 MODE -> TRUE : unique 
-#       -> FALSE : simple
-    if unique == True:
-        unique_vocab=get_uniqueList(final_vocal)
-        return final_vocal
-    return final_vocal
+def preProcessSent(sentence):
+    output_array=[]
+    tokenized_sent=word_tokenize(sentence,format="text").lower()
+    word_array=tokenized_sent.split()
+    with open('vietnamese_dictionary.txt','r',encoding="utf-8") as f:
+        dictionary=f.read().split('\n')
+    for word in word_array:
+        if word in dictionary:
+            output_array.append(word)
+    return output_array
